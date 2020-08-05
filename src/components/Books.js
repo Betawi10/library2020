fc7209c3e042469078ac5c630747b7c7884e445b
Betawi10 <cahyoharli.p@gmail.com>
2020-08-05 17:12:33 +0700

    perubahan 3

---
+++ src/components/Books.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Book from "./Book";
+import PropTypes from "prop-types";
+import {CardColumns} from "reactstrap";
+
+class Books extends Component {
+  render() {
+    return (
+      <CardColumns>
+        {this.props.books.map(book => (
+          <Book 
+          key={book._id} 
+          book={book} 
+          />
+        ))
+        }
+      </CardColumns>
+    )
+  }
+}
+
+//PropTypes
+Books.propTypes = {
+  books: PropTypes.array.isRequired,
+};
+
+export default Books;
---
+++ src/components/ListBooks.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import Books from "./Books";
+import axios from "axios";
+
+class ListBooks extends Component {
+  // TODO: Buat sebuah state berisi books dengan nilai default array kosong []
+  //       dan isLoading dengan nilai default false
+  state = {
+    // isi disini
+    books:[],
+    isLoading:false
+    
+  };
+
+  componentDidMount() {
+    const context = this
+   
+    this.setState({isLoading: true}, () =>
+    axios
+    .get("https://library2020-charlip.herokuapp.com/library")
+    .then(res =>{
+      console.log(res);
+      context.setState({
+        books: res.data,
+        isLoading: false
+      })
+    }
+    )
+    )    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          {this.state.isLoading? <div>Lagi loading...</div>:
+          // TODO: Panggil component BOOKS dengan membawa state books sebagai props books
+          // isi disini
+          <Books
+          books={this.state.books}
+          />
+          }
+        </div>{" "}
+      </div>
+    );
+  }
+}
+
+export default ListBooks;
